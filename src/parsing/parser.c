2a8b2e418aa36915c8442217637838b42b2f7cb7
Haz <hbutt@shi-r4-p2.s19.be>
2024-10-06 13:57:32 +0200

    nothing

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 17:54:51 by hbutt             #+#    #+#             */
-/*   Updated: 2024/10/06 13:52:43 by hbutt            ###   ########.fr       */
+/*   Updated: 2024/10/06 13:56:39 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,19 @@
 
 // 	left = parse_simple_command(token_list);
 
-	if (*token_list && ((*token_list)->type == PIPE
-			|| (*token_list)->type == O_DIR
-			|| (*token_list)->type == OA_DIR
-			|| (*token_list)->type == I_DIR
-			|| (*token_list)->type == DI_DIR))
-	{
-		(*token_list) = (*token_list)->next;
-		t_node *right = parse_command(token_list);
-		return (create_pair_node(left, right));
-			// PIPE devient le nœud parent
-	}
-	return (left); // Si pas de PIPE, retourner juste la commande gauche (la seule)
-}
+// 	if (*token_list && ((*token_list)->type == PIPE
+// 			|| (*token_list)->type == O_DIR
+// 			|| (*token_list)->type == OA_DIR
+// 			|| (*token_list)->type == I_DIR
+// 			|| (*token_list)->type == DI_DIR))
+// 	{
+// 		(*token_list) = (*token_list)->next;
+// 		t_node *right = parse_command(token_list);
+// 		return (create_pair_node(left, right));
+// 			// PIPE devient le nœud parent
+// 	}
+// 	return (left); // Si pas de PIPE, retourner juste la commande gauche (la seule)
+// }
 
 // /**
 //  * Analyse une simple commande 
@@ -48,17 +48,17 @@
 // 	t_node	*root;
 // 	t_node	*new_node;
 
-	if (!*token_list || (*token_list)->type == END_TOKEN)
-		return (NULL);
-	root = create_char_node((*token_list)->lexeme);
-	*token_list = (*token_list)->next;
-	while (*token_list && ((*token_list)->type == CHAR_TOKEN
-			|| (*token_list)->type == SINGLE_QUOTE
-			|| (*token_list)->type == DOUBLE_QUOTE))
-	{
-		new_node = create_char_node((*token_list)->lexeme);
-		root = create_pair_node(root, new_node);
-		*token_list = (*token_list)->next;
-	}
-	return (root);
-}
+// 	if (!*token_list || (*token_list)->type == END_TOKEN)
+// 		return (NULL);
+// 	root = create_char_node((*token_list)->lexeme);
+// 	*token_list = (*token_list)->next;
+// 	while (*token_list && ((*token_list)->type == CHAR_TOKEN
+// 			|| (*token_list)->type == SINGLE_QUOTE
+// 			|| (*token_list)->type == DOUBLE_QUOTE))
+// 	{
+// 		new_node = create_char_node((*token_list)->lexeme);
+// 		root = create_pair_node(root, new_node);
+// 		*token_list = (*token_list)->next;
+// 	}
+// 	return (root);
+// }
