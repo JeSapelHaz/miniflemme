570aaf2ded1f59e8a6427aa0ebc01ac1ad2a65b8
Haz <hbutt@shi-r3-p4.s19.be>
2024-12-04 11:35:54 +0100

    ok

--- src/exe/exe.c
+++ src/exe/exe.c
@@ -6,7 +6,7 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 16:58:05 by alama             #+#    #+#             */
-/*   Updated: 2024/11/29 17:42:00 by hbutt            ###   ########.fr       */
+/*   Updated: 2024/12/03 16:46:57 by hbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,6 @@ void	ft_exe(t_node *node, char **env)
 			if (ft_strncmp(node->data.pair.opera, "<<", 3) == 0)
 				di_to_dir(right, left, end, env);
 		}
-		exit(0);
 	}
 	close(end[1]);
 	close(end[0]);
