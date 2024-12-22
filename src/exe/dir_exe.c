44ae79128470ac69c8d1b97cf96421eb519f392c
Ajit Lama <alama@shi-r3-p6.s19.be>
2024-12-22 16:09:32 +0100

    redir

--- src/exe/dir_exe.c
+++ src/exe/dir_exe.c
@@ -6,7 +6,7 @@
 /*   By: hbutt <hbutt@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:00:04 by alama             #+#    #+#             */
-/*   Updated: 2024/12/17 15:13:11 by alama            ###   ########.fr       */
+/*   Updated: 2024/12/22 16:08:36 by alama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,62 +15,98 @@
 void	input_dir(t_node *right, t_node *left, int *end, char **envp)
 {
 	int	fd;
+	int	child;
+	int	status;
 
-	fd = open(right->data.str, O_RDONLY);
-	if (dup2(fd, STDIN_FILENO) == -1)
-	{
-		perror(right->data.str);
+	child = fork();
+	if (child < 0)
 		exit(1);
+	if (child == 0)
+	{
+		fd = open(right->data.str, O_RDONLY);
+		if (dup2(fd, STDIN_FILENO) == -1)
+		{
+			perror(right->data.str);
+			exit(1);
+		}
+		if (left->type == PAIR_NODE)
+			ft_exe(left, envp, end);
+		else
+			first_process(left, envp);
+		close(fd);
+		dup2(end[0], STDIN_FILENO);
+		dup2(end[1], STDOUT_FILENO);
+		exit(excode);
 	}
-	if (left->type == PAIR_NODE)
-		ft_exe(left, envp, end);
-	else
-		first_process(left, envp);
-	close(fd);
-	dup2(end[0], STDIN_FILENO);
-	dup2(end[1], STDOUT_FILENO);
+	waitpid(child, &status, 0);
+	if (WIFEXITED(status))
+		excode = WEXITSTATUS(status);
 }
 
 void	output_dir(t_node *right, t_node *left, int *end, char **envp)
 {
 	int	fd;
+	int	child;
+	int	status;
 
-	fd = open(right->data.str, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd == -1)
-		perror("open file fails\n");
-	if (dup2(fd, STDOUT_FILENO) == -1)
-	{
-		perror(NULL);
+	child = fork();
+	if (child < 0)
 		exit(1);
+	if (child == 0)
+	{
+		fd = open(right->data.str, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if (fd == -1)
+			perror("open file fails\n");
+		if (dup2(fd, STDOUT_FILENO) == -1)
+		{
+			perror(NULL);
+			exit(1);
+		}
+		if (left->type == PAIR_NODE)
+			ft_exe(left, envp, end);
+		else
+			first_process(left, envp);
+		close(fd);
+		dup2(end[0], STDIN_FILENO);
+		dup2(end[1], STDOUT_FILENO);
+		exit(excode);
 	}
-	if (left->type == PAIR_NODE)
-		ft_exe(left, envp, end);
-	else
-		first_process(left, envp);
-	close(fd);
-	dup2(end[0], STDIN_FILENO);
-	dup2(end[1], STDOUT_FILENO);
+	waitpid(child, &status, 0);
+	if (WIFEXITED(status))
+		excode = WEXITSTATUS(status);
 }
 
 void	output_append(t_node *right, t_node *left, int *end, char **envp)
 {
 	int	fd;
+	int	child;
+	int	status;
 
-	fd = open(right->data.str, O_CREAT | O_WRONLY | O_APPEND, 0664);
-	if (fd == -1)
-		perror("open file fails\n");
-	if (dup2(fd, STDOUT_FILENO) == -1)
-	{
-		perror(NULL);
+	child = fork();
+	if (child < 0)
 		exit(1);
+	if (child == 0)
+	{
+		fd = open(right->data.str, O_CREAT | O_WRONLY | O_APPEND, 0664);
+		if (fd == -1)
+			perror("open file fails\n");
+		if (dup2(fd, STDOUT_FILENO) == -1)
+		{
+			perror(NULL);
+			exit(1);
+		}
+		if (left->type == PAIR_NODE)
+			ft_exe(left, envp, end);
+		else
+			first_process(left, envp);
+		close(fd);
+		dup2(end[0], STDIN_FILENO);
+		dup2(end[1], STDOUT_FILENO);
+		exit(excode);
 	}
-	if (left->type == PAIR_NODE)
-		ft_exe(left, envp, end);
-	else
-		first_process(left, envp);
-	close(fd);
-	dup2(end[0], STDIN_FILENO);
-	dup2(end[1], STDOUT_FILENO);
+	waitpid(child, &status, 0);
+	if (WIFEXITED(status))
+		excode = WEXITSTATUS(status);
 }
 
 void	di_to_dir(t_node *right, t_node *left, int *end, char **envp)
